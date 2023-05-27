d26199a72a8f69f0c39642632c13692bf13a7bb4
“evan-weissrussland” <evanwodichka@gmail.com>
2023-05-27 19:43:36 +0300

    new

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -4,7 +4,7 @@ import MessageSender from './message-sender/MessageSender'
 import s2 from '../../s1-main/App.module.css'
 import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
-sdv
+
 /*
 * 1 - описать тип MessageType                              //выполенено
 * 2 - описать тип MessagePropsType в файле Message.tsx      //выполенено
