8545ef52b6e79c6260361ce9805d4e9264860e4a
“evan-weissrussland” <evanwodichka@gmail.com>
2023-06-30 15:25:30 +0300

    new

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -43,10 +43,14 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
     const onEnterCallback = () => {
         // выключить editMode при нажатии Enter // делают студенты
 
+        setEditMode(false)//я делал
+
         onEnter?.()
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // выключить editMode при нажатии за пределами инпута // делают студенты
+
+        setEditMode(false)//я делал
 
         onBlur?.(e)
     }
@@ -54,6 +58,8 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
         e: React.MouseEvent<HTMLSpanElement, MouseEvent>
     ) => {
         // включить editMode при двойном клике // делают студенты
+
+        setEditMode(true) //я делал
 
         onDoubleClick?.(e)
     }
