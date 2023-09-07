36932fde94175251868c3248049499b4e9df061d
“evan-weissrussland” <evanwodichka@gmail.com>
2023-09-07 21:44:16 +0300

    new

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -36,7 +36,7 @@ function Clock() {
         minute: "numeric",
         second: "numeric"
     }).format(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = new Intl.DateTimeFormat('en-US').format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = new Intl.DateTimeFormat('ru').format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = new Intl.DateTimeFormat('en-US', {weekday:"long"}).format(date) || <br/> // пишут студенты
