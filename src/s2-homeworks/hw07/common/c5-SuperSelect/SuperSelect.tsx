1d2c24952174ddb13e5462da458fc0b43dd9ee42
“evan-weissrussland” <evanwodichka@gmail.com>
2023-10-08 20:21:07 +0300

    new

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -37,7 +37,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // делают студенты
-        onChangeOption&&onChangeOption(Number(e.currentTarget.value))
+        onChangeOption && onChangeOption(Number(e.currentTarget.value))
     }
 
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
